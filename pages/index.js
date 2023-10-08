9b2cfd6765dbd735283e2599217809e6040341db
robinsingh051 <robinsingh051s@gmail.com>
2023-10-08 07:38:09 +0530

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// for route -> /
+
+const HomePage = (props) => {
+  return (
+    <>
+      <h1>The Home Page</h1>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,11 @@
+// for route -> /news
+
+const NewsPage = (props) => {
+  return (
+    <>
+      <h1>The News Page</h1>
+    </>
+  );
+};
+
+export default NewsPage;
