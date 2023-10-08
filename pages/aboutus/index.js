8af505a83c1554126f1f3d251b09efa043115912
robinsingh051 <robinsingh051s@gmail.com>
2023-10-08 08:30:34 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,11 +1,26 @@
 // for route -> /aboutus
+import Link from "next/link";
 
-const NewsPage = (props) => {
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+
+const AboutPage = (props) => {
+  const detailsElement = details.map((item) => {
+    return (
+      <li>
+        <Link href={`/aboutus/${item.id}`}>{item.name}</Link>
+      </li>
+    );
+  });
   return (
     <>
-      <h1>The About Us Page</h1>
+      <h1>Our Developers</h1>
+      <ul>{detailsElement}</ul>
     </>
   );
 };
 
-export default NewsPage;
+export default AboutPage;
